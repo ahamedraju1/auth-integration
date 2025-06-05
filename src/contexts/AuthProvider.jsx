4c567f464d7b8f64dbc3b5e5204b0c77e55e034e
unknown <ahamedraju152@gmail.com>
2025-06-05 15:09:54 +0600

    added a useEffect for stay the data even load the page.datay will stay and it works in inside of useEffect

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,5 +1,5 @@
  
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AuthContext } from './AuthContext';
 import { auth } from '../firebase.init';
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
@@ -11,15 +11,25 @@ const AuthProvider = ( {children} ) => {
         
        }
 
-       onAuthStateChanged (auth, (currentUser)=>{
-        if(currentUser){
-            console.log('has current user', currentUser)
-        }
-        else{
-            console.log('current user', currentUser)
-        }
-       })
+    //    onAuthStateChanged (auth, (currentUser)=>{
+    //     if(currentUser){
+    //         console.log('has current user', currentUser)
+    //     }
+    //     else{
+    //         console.log('current user', currentUser)
+    //     }
+    //    })
         
+    useEffect(()=>{
+       const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+         console.log('current user inside useEffect auth state change ', currentUser) 
+       }) 
+
+       return ()=>{
+        unSubscribe ();
+       }
+    }, [])
+
 
        const signInUser = (email, password)=>{
         return  signInWithEmailAndPassword(auth, email, password)
